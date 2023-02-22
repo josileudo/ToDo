7a994b66fc7edebfee8a2bfd169eea5f1fa88198
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2023-02-22 16:35:23 -0300

    :lipstick: style: Styles config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+import { Header } from './components/header';
+import { Task } from './components/task';
+
+import styles from './App.module.css';
+import './global.css';
 
 export function App() {
 
   return (
     <div>
-      <p>hi</p>
+      <Header />
+      <div className={ styles.wrapper }>
+        <Task />
+      </div>
     </div>
   )
 }
